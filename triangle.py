59b2131bb6c9dd1a76d77ea48c08218862a97179
needosleep <drobych123@gmail.com>
2024-09-22 14:02:41 +0300

    The functions of the file triangle.py were documented

--- triangle.py
+++ triangle.py
@@ -1,5 +1,24 @@
 def area(a, h):
+    '''
+    Returns area of triangle.
+        
+        Parameters:
+            a (int | float): base of triangle
+            h (int | float): height of triangle which is drawn to the base
+
+        Result:
+            area (float): the area of this triangle
+    '''
     return a * h / 2
 
 def perimeter(a, b, c):
+    '''
+    Returns perimeter of triangle.
+
+        Parameters:
+            a, b, c (int | float): sides of a triangle
+
+        Result:
+            perimeter (int | float): the perimeter of this triangle
+    '''
     return a + b + c
