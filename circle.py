ca67fea87f751e56cad7fb23fdae5169342a002c
needosleep <drobych123@gmail.com>
2024-09-22 15:11:23 +0300

    The functions from the first two files square.py and circle.py were also documented

--- circle.py
+++ circle.py
@@ -2,9 +2,11 @@ import math
 
 
 def area(r):
+    '''Takes radius of circle, returns its area'''
     return math.pi * r * r
 
 
 def perimeter(r):
+    '''Takes radius of circle, returns its perimeter'''
     return 2 * math.pi * r
 
--- square.py
+++ square.py
@@ -1,7 +1,9 @@
 
 def area(a):
+    '''Takes side of square, returns its area'''
     return a * a
 
 
 def perimeter(a):
+    '''Takes side of square, returns its perimeter'''
     return 4 * a
