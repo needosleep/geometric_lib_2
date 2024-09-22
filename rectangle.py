39d870ea9f42edf474e508d6210bdf4c9976739d
needosleep <drobych123@gmail.com>
2024-09-22 13:54:01 +0300

    The functions of the file rectangle.py were documented

--- rectangle.py
+++ rectangle.py
@@ -1,5 +1,7 @@
 def area(a, b):
+    '''Takes a and b - sides of a rectangle, returns area of a rectangle'''
     return a * b
 
 def perimeter(a, b):
+    '''Takes a and b - sides of a rectangle, returns perimeter of a rectangle'''
     return 2 * (a + b)
